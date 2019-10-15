4943a9e1eb17301ca74784091631e50f1776aa4f
Zestymcspicy <halbeckerman@yahoo.com>
2019-10-14 21:56:33 -0500

    switch for menu items

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,22 @@
+const links = document.querySelectorAll('.js-link');
+const pages = document.querySelectorAll('.js-page');
+const navbarNav = document.getElementById('navbarNav');
+
 document.querySelector( '.navbar-toggler-icon' ).addEventListener( 'click', function() {
-  console.log('hello')
+  // console.log('hello')
   this.classList.toggle( 'active' );
 });
 
+links.forEach(link => link.addEventListener('click', ()=> {
+  navbarNav.classList.remove('show');
+  // navbarNav.classList.add('collapsing');
+  // setTimeout(()=>{
+  //   navbarNav.classList.remove('collapsing');
+  // }, 1000)
+}))
+
+
+// function toggleMenu
 // Uncomment to enable Bootstrap tooltips
 // https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
 // $(function () { $('[data-toggle="tooltip"]').tooltip(); });
@@ -10,3 +24,13 @@ document.querySelector( '.navbar-toggler-icon' ).addEventListener( 'click', func
 // Uncomment to enable Bootstrap popovers
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
+
+links.forEach(link => {
+  link.addEventListener('click', function(e) {
+  console.log(e);
+  pages.forEach(page => {
+    page.style.display='none'
+  });
+  document.querySelector(`#${e.target.innerHTML}`).style.display= 'block';
+  })
+});
